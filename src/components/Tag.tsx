b9968502791708d352edf40f8102721651e487a7
JaneNanthikan <nanthikan_a@seasiacenter.com>
2023-07-12 14:01:22 +0700

    create Tag component

---
+++ src/components/Tag.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type Prop = {
+  title: string
+}
+
+export default function Tag({title}: Prop) {
+  return (
+    <div className="bg-btn_bg text-btn_text font-bold py-2 px-3 rounded-full mr-2 mb-2">
+      {title}
+    </div>
+  )
+}
